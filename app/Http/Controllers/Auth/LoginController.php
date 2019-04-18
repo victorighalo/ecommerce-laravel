7b1ef4088df0cb17514ed592f3eb7ae889b81cf9
victorighalo <victorighalo@live.com>
2019-04-18 15:02:37 +0100

    Edited htaccess to force https

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -34,6 +35,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['guest', 'https'])->except('logout');
+        $this->middleware('guest')->except('logout');
     }
+
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
@@ -62,7 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \Konekt\Acl\Http\Middleware\RoleMiddleware::class,
-        'https' => \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
