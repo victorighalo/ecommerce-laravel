25766c2cbe1026dde258ea7338662df60b3d81cd
victorighalo <victorighalo@live.com>
2019-04-18 10:51:28 +0100

    Edited htaccess to force https

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,6 +34,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['guest', 'http'])->except('logout');
+        $this->middleware(['guest', 'https'])->except('logout');
     }
 }
