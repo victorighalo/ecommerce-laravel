9af23071c0a632d25e5a809e73320978beaa48f7
victorighalo <victorighalo@live.com>
2019-04-18 10:48:13 +0100

    Edited htaccess to force https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
@@ -63,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \Konekt\Acl\Http\Middleware\RoleMiddleware::class,
+        'https' => \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
