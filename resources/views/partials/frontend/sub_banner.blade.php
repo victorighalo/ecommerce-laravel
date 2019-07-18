8e80e98a80e9aaadfc493bbd643768e1ab16d4ff
victorighalo <victorighalo@live.com>
2019-07-18 11:31:54 +0100

    Modified colors of the frontend

--- resources/views/partials/frontend/sub_banner.blade.php
+++ resources/views/partials/frontend/sub_banner.blade.php
@@ -1,17 +1,17 @@
 <section class="sub-bnr" data-stellar-background-ratio="0.5">
     <div class="position-center-center">
         <div class="container">
-            <h4>{{$title ? $title : ''}}</h4>
+            <h4>{{$title ? ucfirst(strtolower($title)) : ''}}</h4>
             <ol class="breadcrumb">
                 <li><a href="{{url('/')}}">Home</a></li>
                 @if(isset($category))
-                    <li>{{$category->taxonomy->name}}</li>
+                    <li>{{ucfirst(strtolower($category->taxonomy->name))}}</li>
                     @if($category->parent)
-                    <li class="active"><a a href="{{route('get_category_content', ['taxon_slug' => $category->parent->slug])}}" title="{{$category->parent->name}}" >{{$category->parent->name}}</a></li>
+                    <li class="active"><a a href="{{route('get_category_content', ['taxon_slug' => $category->parent->slug])}}" title="{{ucfirst(strtolower($category->parent->name))}}" >{{ucfirst(strtolower($category->parent->name))}}</a></li>
                     @endif
-                    <li class="active"><a a href="{{route('get_category_content', ['taxon_slug' => $category->slug])}}" title="{{$category->name}}" >{{$category->slug}}</a></li>
+                    <li class="active"><a a href="{{route('get_category_content', ['taxon_slug' => $category->slug])}}" title="{{ucfirst(strtolower($category->name))}}" >{{ucfirst(strtolower($category->slug))}}</a></li>
                     @else
-                    <li class="active">{{$title ? $title : ''}}</li>
+                    <li class="active">{{$title ? ucfirst(strtolower($title)) : ''}}</li>
                     @endif
             </ol>
         </div>
