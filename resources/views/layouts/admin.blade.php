99e51fcce71992397db5718cc759c1697eeb97e3
Victor Ighalo <victorighalo@centricgateway.com>
2020-01-06 14:06:12 +0100

    Removed multiple title tags

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
-    {!! SEOMeta::generate(true) !!}
+{{--    {!! SEOMeta::generate(true) !!}--}}
     <!-- Styles -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
