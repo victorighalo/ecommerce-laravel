93a595d9140bd773898fd9bc2b47138ff24167b8
Victor Ighalo <victorighalo@centricgateway.com>
2020-05-25 07:27:12 +0100

    update categories slide

--- resources/views/partials/frontend/categories_slider.blade.php
+++ resources/views/partials/frontend/categories_slider.blade.php
@@ -4,11 +4,11 @@
         <h3 class=" text-center margin-bottom-30">Product Categories</h3>
         <div class="">
             @foreach($all_categories as $category)
-                <div class="d-inline font-weight-bold shadow-sm bg-gray-300 p-2 m-2 rounded cursor-pointer">
+                <span class="font-weight-bold shadow-sm bg-gray-300 p-2 m-2 rounded cursor-pointer">
                 <a href="{{route('get_category_content', ['taxon_slug' => $category->slug])}}" title="{{$category->name}}" class="text-capitalize font-16px; margin-bottom-15">{{$category->name}}
                     <span class='bg-gray-100 p-2 m-2'>{{count($category->products)}}</span>
                 </a>
-                </div>
+                </span>
             @endforeach
         </div>
     </div>
